ce93abe7d3e75cf5fde1f342a4d58d8a1fff5f22
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-02-14 11:34:03 +0100

    Updated User

--- CA1Server/src/main/java/server/User.java
+++ CA1Server/src/main/java/server/User.java
@@ -3,14 +3,17 @@ package server;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * Created by Niki on 2017-02-14.
  *
  * @author Niki
  */
-public class User {
+public class User implements Observer {
 
     private Socket socket;
     private String username;
@@ -19,7 +22,7 @@ public class User {
 
     public User(Socket socket, String username) {
         this.socket = socket;
-        this.username = username;
+        this.username = username.toLowerCase();
     }
 
     public InputStream getInputStream() {
@@ -44,5 +47,19 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        User u = null;
+        if (arg instanceof User)
+            u = (User) arg;
+
+        if (u == null) return;
+
+        PrintWriter writer = new PrintWriter(outputStream);
+        writer.flush();
+        writer.println("UPDATE#" + u.username);
+        writer.flush();
     }
 }
