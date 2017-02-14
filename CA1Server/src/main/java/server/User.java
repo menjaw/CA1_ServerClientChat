bbaae9a15da44bfd57c53c18a7b24b6edec388ee
NikiSkaarup <nikiskaarup@users.noreply.github.com>
2017-02-14 12:06:21 +0100

    Updated user

--- CA1Server/src/main/java/server/User.java
+++ CA1Server/src/main/java/server/User.java
@@ -1,9 +1,6 @@
 package server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Observable;
 import java.util.Observer;
@@ -17,49 +14,41 @@ public class User implements Observer {
 
     private Socket socket;
     private String username;
-    private InputStream inputStream;
-    private OutputStream outputStream;
+    private BufferedReader reader;
+    private PrintStream writer;
 
     public User(Socket socket, String username) {
         this.socket = socket;
         this.username = username.toLowerCase();
-    }
 
-    public InputStream getInputStream() {
         try {
-            if (inputStream == null)
-                inputStream = socket.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new PrintStream(socket.getOutputStream());
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return inputStream;
     }
 
-    public OutputStream getOutputStream() {
-        try {
-            if (outputStream == null)
-                outputStream = socket.getOutputStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return outputStream;
+    @Override
+    public void update(Observable o, Object arg) {
+        User u = null;
+        if (arg instanceof User) u = (User) arg;
+        if (u == null || u == this) return;
+
+        writer.println("UPDATE#" + u.username);
+        writer.flush();
     }
 
     public String getUsername() {
         return username;
     }
 
-    @Override
-    public void update(Observable o, Object arg) {
-        User u = null;
-        if (arg instanceof User)
-            u = (User) arg;
+    public BufferedReader getReader() {
+        return reader;
+    }
 
-        if (u == null || u == this) return;
-
-        PrintWriter writer = new PrintWriter(outputStream);
-        writer.flush();
-        writer.println("UPDATE#" + u.username);
-        writer.flush();
+    public PrintStream getWriter() {
+        return writer;
     }
 }
