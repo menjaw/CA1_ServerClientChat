be038652f7438b4667a72963b27502db4a05d03e
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-02-14 17:24:49 +0100

    started working on OK message at login

--- CA1Server/src/main/java/server/ChatServer.java
+++ CA1Server/src/main/java/server/ChatServer.java
@@ -87,9 +87,11 @@ public class ChatServer extends Observable {
                     String okMsg = "OK#";
                     for (User user : users) {
                         okMsg.concat(user.getUsername() + "#");
+
                     }
                     newGuy.write(okMsg);
                     break;
+
                 case "MSG":
                     //TODO: What happens here now?
                     break;
@@ -110,11 +112,13 @@ public class ChatServer extends Observable {
                     if (msg.getReceiver() == null) {
                         for (User user : users) {
                             user.write(msg.toString());
+
                         }
+
                     } else {
                         msg.getReceiver().write(msg.toString());
-                    }
 
+                    }
                 } catch (InterruptedException ex) {
                     Logger.getLogger(ChatServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
