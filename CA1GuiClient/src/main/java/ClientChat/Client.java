95ec3ab18657414ede48adbf8c6a7cf39f0e9e94
menjaw <menjaholm@gmail.com>
2017-02-16 11:57:44 +0100

    new variables and structure buildt

--- CA1GuiClient/src/main/java/ClientChat/Client.java
+++ CA1GuiClient/src/main/java/ClientChat/Client.java
@@ -8,9 +8,8 @@ package ClientChat;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,82 +22,61 @@ public class Client {
     //GUI
     private GuiClient guiClient;
 
-    //Input (to read from the socket) and Output (to write from the socket)
-    private DataInputStream messageInput;
-    private DataOutputStream messageOutput;
-    private Socket socket;
-
-    //Login
-    private String username;
+    //Server
     private String server;
     private String host;
     private int port;
 
-    /**
-     * Consol constructor called by consol mode In this mode the GUI is set to
-     * null
-     *
-     * @param username
-     * @param server
-     * @param host
-     * @param port
-     */
-    public Client(String username, String server, String host, int port) {
-        this(username, server, host, port, null);
-    }
-
-    /**
-     * GUI constructor called by GUI mode
-     *
-     * @param username
-     * @param server: the address of the server
-     * @param host
-     * @param port: the port number on the server
-     * @param guiClient:
-     */
-    public Client(String username, String server, String host, int port, GuiClient guiClient) {
-        this.username = username;
-        this.server = server;
-        this.host = host;
-        this.port = port;
-        this.guiClient = guiClient;
-    }
-
-    
-    public static void main(String[] args) {
-        
-    }
-    
-    
-//    //Start and connect to the server
-//    public boolean start() {
-//
-//        //connect to server
-//        try {
-//            socket = new Socket(server, port);
-//        } catch (IOException ex) {
-//            //if the connection fail
-//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-//            System.out.println("Connection failed");
-//            return false;
-//        }
-//        //If the connection succeeded
-//        String serverConnected = "Connection succeeded " + "\n" + socket.getInetAddress() + ": " + socket.getPort();
-//        System.out.println(serverConnected);
-//
-//        try {
-//            //creates the data streams for both Input-Objects
-//            messageInput = new DataInputStream(socket.getInputStream());
-//            messageOutput = new DataOutputStream(socket.getOutputStream());
-//        } catch (IOException ex) {
-//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-//            //Creating new input or output streams failed
-//            System.out.println("Creating new input or output streams failed " + ex);
-//            return false;
-//        }
-//
-//        return true;
+//    /**
+//     * Consol constructor called by consol mode In this mode the GUI is set to
+//     * null
+//     *
+//     * @param server
+//     * @param host
+//     * @param port
+//     */
+//    public Client(String server, String host, int port) {
+//        this(server, host, port, null);
 //    }
+//
+//    /**
+//     * GUI constructor called by GUI mode
+//     *
+//     * @param server: the address of the server
+//     * @param host
+//     * @param port: the port number on the server
+//     * @param guiClient:
+//     */
+//    public Client(String server, String host, int port, GuiClient guiClient) {
+//
+//        this.server = server;
+//        this.host = host;
+//        this.port = port;
+//        this.guiClient = guiClient;
+//    }
+    public static void main(String[] args) {
+        try {
+            //Input (to read from the socket) and Output (to write from the socket)
+            Socket socket = new Socket("127.0.0.1", 8081);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            
+            Scanner scanner = new Scanner(dataInputStream);
+            String messageInput = "";
+            String messageOutput = "";
+            
+            while(!messageInput.equals("end")){
+            messageOutput = scanner.nextLine();
+            dataOutputStream.writeUTF(messageOutput);
+            
+            messageInput = dataInputStream.readUTF();
+                System.out.println(messageInput);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     /**
      *
@@ -128,27 +106,54 @@ public class Client {
 
     }
 
-    class ListenFromServer extends Thread {
-
-        @Override
-        public void run() {
-            try {
-                String message = (String) messageInput.readObject();
-                
-                //If it is the console mode this is running
-                if (guiClient == null) {
-                    System.out.println(message);
-                }
-                //If it is the GUI mode
-                //OBS!!!!!! Append findes ikke i denne sammenhæng
-                else{
-                guiClient.add(message, guiClient);
-                }
-            } catch (IOException | ClassNotFoundException ex) {
-                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        
-        
-        }
-    }
 }
+
+//    //Start and connect to the server
+//    public boolean start() {
+//
+//        //connect to server
+//        try {
+//            socket = new Socket(server, port);
+//        } catch (IOException ex) {
+//            //if the connection fail
+//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+//            System.out.println("Connection failed");
+//            return false;
+//        }
+//        //If the connection succeeded
+//        String serverConnected = "Connection succeeded " + "\n" + socket.getInetAddress() + ": " + socket.getPort();
+//        System.out.println(serverConnected);
+//
+//        try {
+//            //creates the data streams for both Input-Objects
+//            messageInput = new DataInputStream(socket.getInputStream());
+//            messageOutput = new DataOutputStream(socket.getOutputStream());
+//        } catch (IOException ex) {
+//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+//            //Creating new input or output streams failed
+//            System.out.println("Creating new input or output streams failed " + ex);
+//            return false;
+//        }
+//
+//        return true;
+//    }
+//class ListenFromServer extends Thread {
+//
+//    @Override
+//    public void run() {
+//        try {
+//            String message = (String) messageInput.readObject();
+//
+//            //If it is the console mode this is running
+//            if (guiClient == null) {
+//                System.out.println(message);
+//            } //If it is the GUI mode
+//            //OBS!!!!!! Append findes ikke i denne sammenhæng
+//            else {
+//                guiClient.add(message, guiClient);
+//            }
+//        } catch (IOException | ClassNotFoundException ex) {
+//            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//}
